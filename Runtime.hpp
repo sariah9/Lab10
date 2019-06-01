25852a22dbeba07a1d602205d244917705b8e426
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-06-01 13:29:49 -0700

    Update Runtime.hpp

--- Runtime.hpp
+++ Runtime.hpp
@@ -1,7 +1,16 @@
+/*********************************************************************
+** Program name: Lab 10 - Algorithm Complexity of Fibonacci Sequences
+** Author: Sariah Bunnell 
+** Date:  June 1 2019
+** Description: Runtime base class header file 
+*********************************************************************/
 #ifndef RUNTIME_HPP
 #define RUNTIME_HPP
+#include <iostream>
 #include <chrono>
 using std::chrono;
+using std::cout;
+using std::endl;
 
 class Runtime
 {
