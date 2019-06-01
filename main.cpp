da99cbf873399f6497cb2a378acd7a921b517156
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-06-01 13:26:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
-#include "Menu.hpp"
+#include "Runtime.hpp"
+#include <climits>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int inputValidation(int min, int max)
+{
+  double input;
+  bool repeatEntry = true;
+  char errorTest;
+  string inputLine;
+  while (repeatEntry) {
+      getline(cin, inputLine);
+      stringstream lineStream(inputLine);
+      if (!(lineStream >> input)) {
+          cout << "Input was not a number. Please try again." << endl;
+      } else if ((input < min) || (input > max)) {
+          cout << "Input out of range. Please try again." << endl;
+      } else if (input - static_cast<int>(input) != 0) {
+          cout << "Input with decimals and not an integer. Please try again." << endl;
+      } else if (lineStream >> errorTest) {
+          cout << "Input with extra symbols and/or characters. Please try again." << endl;
+      } else {
+          repeatEntry = false;
+      }
+  }
+  return input;  
+}
 
 int main()
 {
-
+  int value;
+  cout << "What number would you like to measure? " << endl;
+  value = inputValidation(INT_MIN, INT_MAX);
+  Runtime complexity(value);
+  complexity.calcRuntime();
+  return 0;
 }
